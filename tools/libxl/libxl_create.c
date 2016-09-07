e2ea0e78bb9b01432d0c93635795905131beb878
Xu Han <xu.han@intel.com>
2016-09-07 13:16:29 +0800

    vgt: Fix VGT BAR0 MMIO overlap with NIC ROM.
    * This is a workaround that it will always reserve 32M address
    space after the PCI memory start, if Intel vgt detected. It is in
    order to avoid MMIO address space overlap with certain optional
    ROM of other PCI device.
    * Fix bug 807 & 970.
    
    Signed-off-by: Xu Han <xu.han@intel.com>
    Reviewed-by: Yulei Zhang <yulei.zhang@intel.com>
    Signed-off-by: Niu,Bing <bing.niu@intel.com>

--- tools/libxl/libxl_create.c
+++ tools/libxl/libxl_create.c
@@ -451,7 +451,7 @@ int libxl__domain_build(libxl__gc *gc,
         vments[4] = "start_time";
         vments[5] = GCSPRINTF("%lu.%02d", start_time.tv_sec,(int)start_time.tv_usec/10000);
 
-        localents = libxl__calloc(gc, 9, sizeof(char *));
+        localents = libxl__calloc(gc, 11, sizeof(char *));
         i = 0;
         localents[i++] = "platform/acpi";
         localents[i++] = libxl__acpi_defbool_val(info) ? "1" : "0";
@@ -461,6 +461,10 @@ int libxl__domain_build(libxl__gc *gc,
         localents[i++] = libxl_defbool_val(info->u.hvm.acpi_s4) ? "1" : "0";
         localents[i++] = "platform/acpi_laptop_slate";
         localents[i++] = libxl_defbool_val(info->u.hvm.acpi_laptop_slate) ? "1" : "0";
+        localents[i++] = "platform/vgt";
+        localents[i++] = libxl_defbool_val(info->u.hvm.vgt) ? "1" : "0";
+
+
         if (info->u.hvm.mmio_hole_memkb) {
             uint64_t max_ram_below_4g =
                 (1ULL << 32) - (info->u.hvm.mmio_hole_memkb << 10);
